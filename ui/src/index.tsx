d71940ce9f22b078785be41ef316bd07729f8654
Carol-gutianle <2311601671@qq.com>
2023-08-01 17:23:34 +0800

    add: login

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.less";
-import App from "./App";
+import Login from "./components/login/login"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
 );
