19022977cfb31aa75984d0bcb663a17df2fa36a9
MorningForest <2297662686@qq.com>
2023-08-12 18:23:25 +0800

    fix

--- ui/src/components/manager/manager.tsx
+++ ui/src/components/manager/manager.tsx
@@ -54,6 +54,7 @@ function Manager() {
           if(models)
             initSession[models[i].model_id] = []
         }
+        console.log("addchat", newItem.id)
         localStorage.setItem(newItem.id, JSON.stringify(initSession))
     }
 
